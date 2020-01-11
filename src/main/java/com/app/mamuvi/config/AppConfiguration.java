7aa8ea38cf397828fcdde3db24515a1d38b271ca
Pham Van Lam <phamvanlam1991@gmail.com>
2020-01-11 08:38:12 +0700

    show index page

---
+++ src/main/java/com/app/mamuvi/config/AppConfiguration.java
@@ -0,0 +1,30 @@
+package com.app.mamuvi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.app.mamuvi")
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void configureViewResolvers(ViewResolverRegistry registry) {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setViewClass(JstlView.class);
+    viewResolver.setPrefix("/WEB-INF/views/");
+    viewResolver.setSuffix(".jsp");
+    registry.viewResolver(viewResolver);
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+  }
+}
---
+++ src/main/java/com/app/mamuvi/config/AppInitializer.java
@@ -0,0 +1,27 @@
+package com.app.mamuvi.config;
+
+import javax.servlet.Filter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    return new Class[] {AppConfiguration.class};
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return null;
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[] {"/"};
+  }
+
+  @Override
+  protected Filter[] getServletFilters() {
+    Filter[] singleton = {new CORSFilter()};
+    return singleton;
+  }
+}
