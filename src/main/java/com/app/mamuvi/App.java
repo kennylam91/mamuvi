ffec4b6bdb58e5efbe59d63a58d74416c96ebbdd
Pham Van Lam <phamvanlam1991@gmail.com>
2020-01-11 08:26:47 +0700

    first commit + pom

---
+++ src/main/java/com/app/mamuvi/App.java
@@ -0,0 +1,13 @@
+package com.app.mamuvi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
